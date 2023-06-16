4e536e19530ab2a92f19e10ce00850aebe4cdfe5
William <wllm.francis@gmail.com>
2023-06-16 14:41:16 -0400

    mysql downloads twice.

--- mysql/run.sh
+++ mysql/run.sh
@@ -6,15 +6,6 @@ then
 	exit 1
 fi
 DATE=${1}
-rm mysql-latest.tar
-rm -rf sde*
-rm *.sql
-
-curl -k -o mysql-latest.tar.bz2 https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2
-bzip2 -d mysql-latest.tar.bz2
-tar -xvf mysql-latest.tar
-mv sde*/*.sql .
-# docker buildx build -t fallenreaper/eve-mysql:$DATE --platform linux/amd64,linux/arm64,linux/arm/v7 --push  -t fallenreaper/eve-mysql .
 
 docker build -t fallenreaper/eve-mysql:latest .
 docker tag fallenreaper/eve-mysql:latest fallenreaper/eve-mysql:$DATE
